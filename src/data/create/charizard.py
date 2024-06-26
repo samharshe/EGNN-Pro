663ed6bb2e488f16e4dd40f3089edf11a7434e0b
samharshe <samharshe29@gmail.com>
2024-06-26 13:54:46 -0400

    this will not run because no storage on machine. cleaning files and restarting.

---
+++ src/data/create/charizard.py
@@ -0,0 +1,54 @@
+from typing import Any
+import torch
+from torch_geometric.datasets import MD17
+import torch_geometric.transforms as T
+from torch_geometric.transforms import RadiusGraph, NormalizeScale
+
+# minumum energy in benzene, uracil, and aspirin datasets is -406757.5938
+max_abs_energy = -406757.5938
+
+class NormalizeForce(T.BaseTransform):
+    def __call__(self, data: Any) -> Any:
+        data.force = torch.div(data.force, max_abs_energy)
+        
+        return data
+    
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}()'
+
+class NormalizeEnergy(T.BaseTransform):
+    def __call__(self, data: Any) -> Any:
+        data.energy = torch.div(data.energy, max_abs_energy)
+        
+        return data
+    
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}()'
+
+class DoubleDistance(T.BaseTransform):
+    def __call__(self, data: Any) -> Any:
+        data.pos = data.pos*2
+        
+        return data
+    
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}()'
+    
+class MakeEdgeAttributes(T.BaseTransform):
+    def __call__(self, data: Any) -> Any:
+        idx1 = data.edge_index[0]
+        idx2 = data.edge_index[1]
+        data.edge_vectors = data.pos[idx1] - data.pos[idx2]
+        data.edge_vectors_length = torch.norm(data.edge_vectors, dim=1)
+        data.unit_edge_vectors = torch.div(data.edge_vectors, data.edge_vectors_length.view(-1,1))
+        
+        return data
+    
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}()'
+
+transform = T.Compose([RadiusGraph(1.8100), NormalizeScale(), DoubleDistance(), NormalizeEnergy(), NormalizeForce(), MakeEdgeAttributes()])
+
+benzene_dataset = MD17(root='/Users/samharshe/Documents/Gerstein Lab/EGNN Pro/data/charizard/', name='benzene', pre_transform=transform)
+uracil_dataset = MD17(root='/Users/samharshe/Documents/Gerstein Lab/EGNN Pro/data/charizard/', name='uracil', pre_transform=transform)
+aspirin_dataset = MD17(root='/Users/samharshe/Documents/Gerstein Lab/EGNN Pro/data/charizard/', name='aspirin', pre_transform=transform)
